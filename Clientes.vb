a82fc4f0b3e2e90ad78dd4d90b90d35484cf0979
CARLOS ZHONG <carlos.zhong@utp.ac.pa>
2021-07-23 03:30:56 -0500

    Push clientes 2

--- Clientes.vb
+++ Clientes.vb
@@ -2,6 +2,12 @@
 Public Class Clientes
     Private Sub Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         BuscarCB.SelectedIndex = 0
+        Dim temp As New DataSet
+        Dim adaptador As New SqlDataAdapter("Select *
+                                                     from Usuario", gymbokuform.cn)
+
+        adaptador.Fill(temp, "Usuario")
+        ClienteDGV.DataSource = temp.Tables("Usuario")
     End Sub
 
     Private Sub BuscarBtn_Click(sender As Object, e As EventArgs) Handles BuscarBtn.Click
@@ -9,12 +15,11 @@ Public Class Clientes
             If (BuscarTxtbx.Text.Trim = "") Then
                 MsgBox("No puede dejar el código de producto en blanco")
             Else
-                Dim temp As New DataSet
+
                 Dim adaptador As New SqlDataAdapter("Select *
                                                      from Producto where ID_Producto = '" & BuscarTxtbx.Text & "'", gymbokuform.cn)
 
-                adaptador.Fill(temp, "Suscripcion")
-                ' ProdDGV.DataSource = temp.Tables("Suscripcion")
+
 
             End If
         Catch ex As Exception
