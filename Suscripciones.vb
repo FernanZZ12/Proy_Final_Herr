160925c3e73a6c192ff844d456cf511c3eed2c85
CARLOS ZHONG <carlos.zhong@utp.ac.pa>
2021-07-23 06:16:40 -0500

    PUSH FINAL

--- Suscripciones.vb
+++ Suscripciones.vb
@@ -17,4 +17,22 @@ Public Class Suscripciones
             MsgBox("Error")
         End Try
     End Sub
+
+    Private Sub ModificarSusBtn_Click(sender As Object, e As EventArgs) Handles ModificarSusBtn.Click
+        Dim adaptador As New SqlDataAdapter("Select * from Suscripcion where ID_Suscripcion = '" & CodigoTxtBx.Text & "'", gymbokuform.cn)
+        Dim ds As New DataSet
+        adaptador.Fill(ds, "Suscripcion")
+
+        If (Suscripciontxtbx.Text = "" Or Descripciontxtbx.Text = "" Or Descuentotxtbx.Text = "") Then
+            MsgBox("No puede dejar espacios en blanco")
+        Else
+            If ds.Tables("Suscripcion").Rows.Count > 0 Then
+                Dim adaptador1 As New SqlCommand("update Suscripcion set Nombre_Suscripcion ='" & Suscripciontxtbx.Text & "', Descripcion ='" & Descripciontxtbx.Text & "',  Descuento ='" & Descuentotxtbx.Text & "' Where ID_Suscripcion = '" & CodigoTxtBx.Text & "'", gymbokuform.cn)
+                adaptador1.ExecuteNonQuery()
+                MsgBox("Se ha cambiado exitosamente")
+            Else
+                MsgBox("No existe este producto.")
+            End If
+        End If
+    End Sub
 End Class
