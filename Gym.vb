bd957cafef0b72601c154df0b892baec53619c08
Fernando Zeng <fernando.zheng@hotmail.com>
2021-07-23 01:10:47 -0500

    Conexion Establecida

--- Gym.vb
+++ Gym.vb
@@ -1,2 +1,30 @@
-﻿Public Class gymbokuform
+﻿Imports System.Data.SqlClient
+Public Class gymbokuform
+    Public cn As New SqlConnection
+    Private Sub ClientesItem_Click(sender As Object, e As EventArgs) Handles ClientesItem.Click
+        Clientes.Show()
+    End Sub
+
+    Private Sub SuscripcionesItem_Click(sender As Object, e As EventArgs) Handles SuscripcionesItem.Click
+        Suscripciones.Show()
+    End Sub
+
+    Private Sub ProductosItem_Click(sender As Object, e As EventArgs) Handles ProductosItem.Click
+        Productos.Show()
+    End Sub
+
+    Private Sub EntrenadoresItem_Click(sender As Object, e As EventArgs) Handles EntrenadoresItem.Click
+        Entrenadores.Show()
+    End Sub
+
+    Private Sub gymbokuform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim conexion As String
+            conexion = "Server=tcp:bdfinal.database.windows.net,1433;Initial Catalog=Gimnasio;Persist Security Info=False;User ID=fadmin;Password=71188416Zh@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            cn.ConnectionString = conexion
+            cn.Open()
+        Catch ex As Exception
+            MsgBox("No se pudo establecer conexion con la base de datos")
+        End Try
+    End Sub
 End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proy_Final_Herr.Form1
+            Me.MainForm = Global.Proy_Final_Herr.gymbokuform
         End Sub
     End Class
 End Namespace
