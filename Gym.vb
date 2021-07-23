815464f2dd5e53acdbac5ab3b13be1a07bd15aa6
Fernando Zeng <fernando.zheng@hotmail.com>
2021-07-23 06:06:04 -0500

    Factura Completo

--- Gym.vb
+++ Gym.vb
@@ -31,6 +31,12 @@ Public Class gymbokuform
                 .Columns.Add("Precio", System.Type.GetType("System.Double"))
                 .Columns.Add("Subtotal", System.Type.GetType("System.Double"))
             End With
+            Cantidadtxtbx.Text = 1
+            Dim temp As New DataSet
+            Dim adaptador As New SqlDataAdapter("Select *
+                                                 from Factura", cn)
+            adaptador.Fill(temp, "Facturas")
+            numlbl.Text = temp.Tables("Facturas").Rows.Count + 1
         Catch ex As Exception
             MsgBox("No se pudo establecer conexion con la base de datos")
         End Try
@@ -48,35 +54,44 @@ Public Class gymbokuform
                 ElseIf (Cantidadtxtbx.Text.Trim = "" Or Cantidadtxtbx.Text = "0" And CodProtxtbx.Text.Trim = "") Then
                     MsgBox("Error en datos suministrados para ingresar productos")
                 Else
-                    Dim temp As New DataSet
-                    Dim adaptador As New SqlDataAdapter("Select ID_Producto,
-                                                                Descripcion,
-                                                                Precio
-                                                         from Producto where ID_Producto = '" & CodProtxtbx.Text & "'", cn)
-                    adaptador.Fill(temp, "Producto")
-                    Dim newrow As DataRow = detalles.NewRow
-                    newrow("ID_Producto") = temp.Tables("Producto").Rows(0).Item(0)
-                    newrow("Descripcion") = temp.Tables("Producto").Rows(0).Item(1)
-                    newrow("Cantidad") = Convert.ToSingle(Cantidadtxtbx.Text.ToString)
-                    newrow("Precio") = temp.Tables("Producto").Rows(0).Item(2)
-                    newrow("Subtotal") = Math.Round(Convert.ToSingle(Cantidadtxtbx.Text.ToString) * temp.Tables("Producto").Rows(0).Item(2), 2)
-                    detalles.Rows.Add(newrow)
-
-                    Dim adaptador2 As New SqlDataAdapter("select u.ID_Usuario,
+                    If verifica() Then
+                        Dim temp As New DataSet
+                        Dim adaptador2 As New SqlDataAdapter("select u.ID_Usuario,
                                                                 u.Nombre as Nombre_de_Usuario,
                                                                 s.Nombre_Suscripcion as Suscripcion,
                                                                 s.Descuento as Descuento_por_Suscripcion
                                                          from Usuario u inner join Suscripcion s on u.ID_Suscripcion = s.ID_Suscripcion
                                                          where u.ID_Usuario = '" & Idusutxtbx.Text & "'", cn)
-                    adaptador2.Fill(temp, "Usuario")
-                    Dim Subtotal As Double = Sumar()
-                    Dim Descuento As Double = Math.Round(Subtotal * Convert.ToDouble(temp.Tables("Usuario").Rows(0).Item(3)), 2)
-                    Subtotaltxtbox.Text = Subtotal
-                    Desctxtbx.Text = Descuento
-                    Totaltxtbx.Text = Subtotal - Descuento
-                    Cantidadtxtbx.Text = 1
-                    CodProtxtbx.Text = ""
-                    FacturaDGV.DataSource = detalles
+                        adaptador2.Fill(temp, "Usuario")
+                        If temp.Tables("Usuario").Rows.Count > 0 Then
+                            Dim adaptador As New SqlDataAdapter("Select ID_Producto,
+                                                                Descripcion,
+                                                                Precio
+                                                         from Producto where ID_Producto = '" & CodProtxtbx.Text & "'", cn)
+                            adaptador.Fill(temp, "Producto")
+                            Dim newrow As DataRow = detalles.NewRow
+                            newrow("ID_Producto") = temp.Tables("Producto").Rows(0).Item(0)
+                            newrow("Descripcion") = temp.Tables("Producto").Rows(0).Item(1)
+                            newrow("Cantidad") = Convert.ToSingle(Cantidadtxtbx.Text.ToString)
+                            newrow("Precio") = temp.Tables("Producto").Rows(0).Item(2)
+                            newrow("Subtotal") = Math.Round(Convert.ToSingle(Cantidadtxtbx.Text.ToString) * temp.Tables("Producto").Rows(0).Item(2), 2)
+                            detalles.Rows.Add(newrow)
+
+                            Dim Subtotal As Double = Sumar()
+                            Dim Descuento As Double = Math.Round(Subtotal * Convert.ToDouble(temp.Tables("Usuario").Rows(0).Item(3)), 2)
+                            Subtotaltxtbox.Text = Subtotal
+                            Desctxtbx.Text = Descuento
+                            Totaltxtbx.Text = Subtotal - Descuento
+                            Cantidadtxtbx.Text = 1
+                            CodProtxtbx.Text = ""
+                            FacturaDGV.DataSource = detalles
+                        Else
+                            MsgBox("El Cliente no existe")
+                        End If
+                    Else
+                        MsgBox("No puede pagar mas de 1 suscripcion")
+                    End If
+
                 End If
             Catch ex As Exception
                 MsgBox("Error")
@@ -91,4 +106,64 @@ Public Class gymbokuform
         Next
         Return Total
     End Function
+
+    Function verifica() As Boolean
+        Dim temp As New DataSet
+        Dim adaptador2 As New SqlDataAdapter("select *
+                                                From Producto
+                                                where ID_Categoria = 'C02'", cn)
+        adaptador2.Fill(temp, "Producto")
+        For i As Integer = 0 To detalles.Rows.Count - 1
+            For k As Integer = 0 To temp.Tables("Producto").Rows.Count - 1
+                If (detalles.Rows(i).Item(0) = temp.Tables("Producto").Rows(k).Item(0)) Then
+                    Return False
+                End If
+            Next
+        Next
+        Return True
+    End Function
+
+    Private Sub AgregFactbttn_Click(sender As Object, e As EventArgs) Handles AgregFactbttn.Click
+        If detalles.Rows.Count > 0 Then
+            Dim temp As New DataSet
+            If verifica() = False Then
+                Dim adaptador4 As New SqlDataAdapter("select *
+                                                         from Usuario
+                                                         where ID_Usuario = '" & Idusutxtbx.Text & "'", cn)
+                adaptador4.Fill(temp, "Usuarios")
+                If (Convert.ToString(temp.Tables("Usuarios").Rows(0).Item(4)) = "1900-01-01") Then
+                    Dim fecha As Date = System.DateTime.Now
+
+                    Dim adaptador5 As New SqlCommand("update Usuario set Fecha_Expira ='" & fecha.AddMonths(1).ToString(" yyyy/MM/dd") & "' Where ID_Usuario = '" & Idusutxtbx.Text & "'", cn)
+                    adaptador5.ExecuteNonQuery()
+                Else
+                    Dim fecha As Date = System.DateTime.Now
+                    fecha.AddMonths(1)
+                    Dim adaptador5 As New SqlCommand("update Usuario set Fecha_Expira ='" & fecha.AddMonths(1).ToString(" yyyy/MM/dd") & "' Where ID_Usuario = '" & Idusutxtbx.Text & "'", cn)
+                    adaptador5.ExecuteNonQuery()
+                End If
+            End If
+            Dim adaptador As New SqlCommand("insert into Factura values('" & numlbl.Text & "','" & Idusutxtbx.Text & "','" & System.DateTime.Now.ToString((" yyyy/MM/dd")) & "')", cn)
+            adaptador.ExecuteNonQuery()
+
+            For i As Integer = 0 To detalles.Rows.Count - 1
+                Dim adaptador3 As New SqlCommand("insert into Detalle values('" & numlbl.Text & "','" & detalles.Rows(i).Item(0) & "','" & detalles.Rows(i).Item(2) & "','" & detalles.Rows(i).Item(3) & "','" & Convert.ToDouble(Desctxtbx.Text.ToString) / Convert.ToDouble(Subtotaltxtbox.Text.ToString) & "')", cn)
+                adaptador3.ExecuteNonQuery()
+            Next
+
+            Dim adaptador2 As New SqlDataAdapter("Select *
+                                                 from Factura", cn)
+            adaptador2.Fill(temp, "Facturas")
+            numlbl.Text = temp.Tables("Facturas").Rows.Count + 1
+
+            Idusutxtbx.Text = ""
+            detalles.Clear()
+            FacturaDGV.DataSource = detalles
+            Subtotaltxtbox.Text = ""
+            Desctxtbx.Text = ""
+            Totaltxtbx.Text = ""
+        Else
+            MsgBox("No hay factura para ingresar")
+        End If
+    End Sub
 End Class
